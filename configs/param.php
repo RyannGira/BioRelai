4351c070bccb39212f872213943db9b51b293066
BioRelai <giraryann@outlook.fr>
2017-11-23 09:58:22 +0100

    debut modif

--- configs/param.php
+++ configs/param.php
@@ -7,6 +7,6 @@ class Param {
 	// mettre $pass=''
   public static $pass = 'root';
 	// supprimer :8889 de $dsn car numero du port pour mac
-	public static $dsn = 'mysql:host=127.0.0.1:8889;dbname=perrint_vlib;charset=utf8';
+	public static $dsn = 'mysql:host=127.0.0.1:8889;dbname=forea_BioRelai;charset=utf8';
 }
 ?>
