cbb986c11c9e1b68efc65bdccd58ace1cb124ea0
RyannGira <giraryann@outlook.fr>
2017-12-07 08:48:33 +0100

    modif

--- vues/haut.php
+++ vues/haut.php
@@ -4,14 +4,14 @@
 
 <div class='bandeau'>
 	<div class="logo">
-        <a href="index.php?vlibMP=accueil">
-        <img src="images/icon-vlib.png">
+        <a href="index.php?bioRelaiMP=accueil">
+        <img src="images/search-icon.png">
         </a>
     </div>
     <div class="bienvenue">
         <?php
         if(isset($_SESSION['identification'])){
-            echo "<a href='?vlibMP=MonCompte'><p>Bienvenue</p><p>" . $_SESSION['identification'] . "</p></a>";
+            echo "<a href='?bioRelaiMP=MonCompte'><p>Bienvenue</p><p>" . $_SESSION['identification'] . "</p></a>";
         }
         ?>
     </div>
