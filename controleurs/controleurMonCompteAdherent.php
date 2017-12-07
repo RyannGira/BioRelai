6372cc4a7ee347160b091b5c5f88b3eb184c3119
RyannGira <giraryann@outlook.fr>
2017-12-07 09:41:35 +0100

    modif

--- controleurs/controleurMonCompteAdherent.php
+++ controleurs/controleurMonCompteAdherent.php
@@ -1,7 +1,9 @@
 <?php
 $menuAdherent = new Menu("menuSecondaire");
 
-$menuAdherent->ajouterComposant($bioRelaiMP->creerItemLien("MonCompteResponsable", "Mon compte"));
+$menuAdherent->ajouterComposant($menuAdherent->creerItemLien("ModifierCompte", "Modifier mes infos"));
+$menuAdherent->ajouterComposant($menuAdherent->creerItemLien("ConsulterFactures", "Consulter mes factures"));
+$menuAdherent->ajouterComposant($menuAdherent->creerItemLien("MonPanier", "Mon panier"));
 
 $menuSecondaire = $menuAdherent->creerMenu($_SESSION['bioRelaiMP'],'menuAdherent');
 
--- vues/MonCompteAdherent.php
+++ vues/MonCompteAdherent.php
@@ -7,6 +7,13 @@
 
     <div id="content">
 
+        <nav class="menuSecondaire">
+            <?php
+            echo $menuSecondaire;
+            ?>
+        </nav>
+
+
 
         <div id="bas">
             <?php  include 'bas.php' ;?>
